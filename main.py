2c3c5b6683db6ae7954a9f80978187732bfa7445
WDC144 <willcolletti@gmail.com>
2023-03-19 14:08:03 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from turtle import *
+color("green")
+begin_fill()
+pensize(6)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
+
